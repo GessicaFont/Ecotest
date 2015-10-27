43bbfde1a41a205ad9846a2c91c01604f5b3b619
Arthurol <arthur.lopes@uniriotec.br>
2015-10-27 14:34:41 -0200

    Create Trab1.1.java

---
+++ Trab1.1.java
@@ -0,0 +1,16 @@
+package trab;
+
+
+import java.io.FileNotFoundException;
+
+
+public class Trab {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Menu menuJogo = new Menu(); 
+        menuJogo.mostraMenu();
+
+    }
+
+}
