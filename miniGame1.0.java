6721f373edc355e1fe287e8d9ea492cdc792bc06
GessicaFont <gessica.torquato@uniriotec.br>
2015-10-27 13:55:43 -0200

    Create miniGame1.0.java

---
+++ miniGame1.0.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab;
+
+/**
+ *
+ * @author Arthur
+ */
+public class miniGame {
+    
+    
+    boolean iniciaMiniGame(){
+        System.out.println("Teste de início de um mini-game");
+        return true;
+    }
+}
